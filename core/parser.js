66a491ca81ff2d61bbd954bd22416a714b4efe97
Yifei Zhou <austinchou0126@gmail.com>
2019-11-25 12:01:08 +0800

    Better code separation

---
+++ core/parser.js
@@ -0,0 +1,150 @@
+const splitByCommas = string => (string || "").split(/,\s*/).map(e => e.trim());
+
+function parseHTTPProxyLine(name, confLine, option = {}) {
+  let i = 1;
+  const params = splitByCommas(confLine);
+  const protocol = 'http';
+  const proxyConf = {
+    name,
+    type: protocol,
+    server: params[i++],
+    port: parseInt(params[i++], 10)
+  };
+  if (option.tls) {
+    proxyConf.tls = true;
+  }
+
+  if (params[i]) {
+    proxyConf.username = params[i++];
+  }
+
+  if (params[i]) {
+    proxyConf.password = params[i++];
+  }
+
+  if (params.length > i) {
+    for (let { length } = params; i < length; i++) {
+      const [key, value] = params[i].split('=');
+      // eslint-disable-next-line max-depth
+      if (key === 'skip-cert-verify') {
+        proxyConf['skip-cert-verify'] = value;
+      }
+    }
+  }
+
+  return proxyConf;
+}
+
+function parseSOCKSProxyLine(name, confLine, option = {}) {
+  let i = 1;
+  const params = splitByCommas(confLine);
+  protocol = 'socks5';
+
+  proxyConf = {
+    name,
+    type: protocol,
+    server: params[i++],
+    port: params[i++]
+  };
+  if (option.tls) {
+    proxyConf.tls = true;
+  }
+
+  if (params[i]) {
+    proxyConf.username = params[i++];
+  }
+
+  if (params[i]) {
+    proxyConf.password = params[i++];
+  }
+
+  if (params.length > i) {
+    for (let { length } = params; i < length; i++) {
+      const [key, value] = params[i].split('=');
+      // eslint-disable-next-line max-depth
+      if (key === 'skip-cert-verify') {
+        proxyConf['skip-cert-verify'] = value;
+      }
+    }
+  }
+
+  return proxyConf;
+}
+
+function parseCustomProxyLine(name, confLine, option = {}) {
+  let i = 1;
+  const params = splitByCommas(confLine);
+  const protocol = 'ss';
+  return {
+    name,
+    type: protocol,
+    server: params[i++],
+    port: parseInt(params[i++], 10),
+    cipher: params[i++],
+    password: params[i++]
+  }
+}
+
+function parseProxy(surgeConf, options = {}) {
+  const proxys = [];
+  Object.keys(surgeConf.Proxy).map(k => {
+    const e = surgeConf.Proxy[k];
+    let proxyConf;
+    let option = {};
+    switch (splitByCommas(e)[0]) {
+      case 'custom':
+        proxyConf = parseCustomProxyLine(k, e);
+        break;
+      case 'https':
+        option.tls = true;
+      case 'http':
+        proxyConf = parseHTTPProxyLine(k, e, option);
+        break;
+      case 'socks5-tls':
+        option.tls = true;
+      case 'socks5':
+        proxyConf = parseSOCKSProxyLine(k, e, option);
+        break;
+    }
+    if (options.stringify) {
+      proxys.push(proxyConf);
+    } else {
+      proxys.push(JSON.stringify(proxyConf));
+    }
+  })
+  
+  return proxys;
+}
+
+function parseProxyGroup(surgeConf, option = {}) {
+  const proxyGroups = [];
+  const reservedKeywords = ['DIRECT'];
+  
+  for (const name of Object.keys(surgeConf['Proxy Group'])) {
+    const group = splitByCommas(surgeConf['Proxy Group'][name]);
+    const each = {
+      name,
+      type: group[0]
+    };
+    const proxies = [];
+    for (let i = 1; i < group.length; i++) {
+      if (Object.keys(surgeConf.Proxy).includes(group[i]) || // ref from proxy section
+        reservedKeywords.includes(group[i]) || // ref from reserved keywords
+        Object.keys(surgeConf['Proxy Group']).includes(group[i])) { // ref from previous proxy group
+        proxies.push(group[i]);
+      } else if (group[i].indexOf('=') !== -1) {
+        const [key, val] = group[i].split(/\s*=\s*/);
+        each[key] = val;
+      }
+    }
+
+    each.proxies = proxies;
+    proxyGroups.push(each);
+  }
+  return proxyGroups;
+}
+
+module.exports = {
+  parseProxy,
+  parseProxyGroup,
+}
--- core/surge2clash.js
+++ core/surge2clash.js
@@ -1,8 +1,8 @@
 /* eslint no-fallthrough: 0 */
 const ini = require('js-ini');
 const jsYaml = require('js-yaml');
+const { parseProxy, parseProxyGroup } = require('./parser');
 
-const splitByCommas = string => (string || "").split(/,\s*/).map(e => e.trim());
 const isTrue = value => typeof value === 'boolean' ? value : value == 'true';
 
 const defaultClashConf = {
@@ -17,138 +17,6 @@ const defaultClashConf = {
 }
 
 const clashConf = Object.assign({}, defaultClashConf);
-
-function parseProxy(surgeConf, options = {}) {
-  const proxys = [];
-  for (const name of Object.keys(surgeConf.Proxy)) {
-    const proxy = splitByCommas(surgeConf.Proxy[name]);
-    let tls = false;
-    let proxyConf;
-    let protocol;
-    let i = 1;
-    // eslint-disable-next-line default-case
-    switch (proxy[0]) {
-      case 'custom':
-        protocol = 'ss';
-        proxys.push({
-          name,
-          type: protocol,
-          server: proxy[i++],
-          port: parseInt(proxy[i++], 10),
-          cipher: proxy[i++],
-          password: proxy[i++]
-        });
-        break;
-      case 'https':
-        protocol = 'http';
-        tls = true;
-      case 'http':
-        protocol = 'http';
-        proxyConf = {
-          name,
-          type: protocol,
-          server: proxy[i++],
-          port: parseInt(proxy[i++], 10)
-        };
-        if (tls) {
-          proxyConf.tls = true;
-        }
-
-        if (proxy[i]) {
-          proxyConf.username = proxy[i++];
-        }
-
-        if (proxy[i]) {
-          proxyConf.password = proxy[i++];
-        }
-
-        if (proxy.length > i) {
-          for (let {length} = proxy; i < length; i++) {
-            const [key, value] = proxy[i].split('=');
-            // eslint-disable-next-line max-depth
-            if (key === 'skip-cert-verify') {
-              proxyConf['skip-cert-verify'] = value;
-            }
-          }
-        }
-
-        if (options.win) {
-          proxys.push(proxyConf);
-        } else {
-          proxys.push(JSON.stringify(proxyConf));
-        }
-
-        break;
-      case 'socks5-tls':
-        protocol = 'socks5';
-        tls = true;
-      case 'socks5':
-        protocol = 'socks5';
-        proxyConf = {
-          name,
-          type: protocol,
-          server: proxy[i++],
-          port: proxy[i++]
-        };
-        if (tls) {
-          proxyConf.tls = true;
-        }
-
-        if (proxy[i]) {
-          proxyConf.username = proxy[i++];
-        }
-
-        if (proxy[i]) {
-          proxyConf.password = proxy[i++];
-        }
-
-        if (proxy.length > i) {
-          for (let {length} = proxy; i < length; i++) {
-            const [key, value] = proxy[i].split('=');
-            // eslint-disable-next-line max-depth
-            if (key === 'skip-cert-verify') {
-              proxyConf['skip-cert-verify'] = value;
-            }
-          }
-        }
-
-        if (options.win) {
-          proxys.push(proxyConf);
-        } else {
-          proxys.push(JSON.stringify(proxyConf));
-        }
-    }
-  }
-  return proxys;
-}
-
-function parseProxyGroup(surgeConf, option = {}) {
-  const proxyGroups = [];
-  const reservedKeywords = ['DIRECT'];
-  
-  for (const name of Object.keys(surgeConf['Proxy Group'])) {
-    const group = splitByCommas(surgeConf['Proxy Group'][name]);
-    const each = {
-      name,
-      type: group[0]
-    };
-    const proxies = [];
-    for (let i = 1; i < group.length; i++) {
-      if (Object.keys(surgeConf.Proxy).includes(group[i]) || // ref from proxy section
-        reservedKeywords.includes(group[i]) || // ref from reserved keywords
-        Object.keys(surgeConf['Proxy Group']).includes(group[i])) { // ref from previous proxy group
-        proxies.push(group[i]);
-      } else if (group[i].indexOf('=') !== -1) {
-        const [key, val] = group[i].split(/\s*=\s*/);
-        each[key] = val;
-      }
-    }
-
-    each.proxies = proxies;
-    proxyGroups.push(each);
-  }
-  return proxyGroups;
-}
 
 // eslint-disable-next-line complexity
 function surge2Clash(surgeConfText, query) {
@@ -166,12 +34,12 @@ function surge2Clash(surgeConfText, query) {
 
   dns.nameserver = surgeConf.General['dns-server'].split(/,\s+/).filter(i => i.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/) || i.match(/^\w+:\/\//));
   dns.ipv6 = isTrue(surgeConf.General.ipv6);
-  if(!query.win) {
+  if (!query.win) {
     clashConf.dns = dns;
   }
 
-  const proxys = parseProxy(surgeConf, query) || [];
-  const proxyGroups = parseProxyGroup(surgeConf, query) || [];
+  const proxys = parseProxy(surgeConf, { stringify: !!query.win }) || [];
+  const proxyGroups = parseProxyGroup(surgeConf, { stringify: !!query.win }) || [];
 
   clashConf.Proxy = proxys;
   clashConf['Proxy Group'] = proxyGroups;
